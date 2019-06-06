c38fb88118f30d9a788cb527a7924c7fbb8a577d
Liora <lioralehrer@gmail.com>
2019-06-07 00:23:32 +0300

    the painter can paint now

--- js/painter.js
+++ js/painter.js
@@ -1,12 +1,10 @@
 var canvas = document.getElementById("canvas");
-var drawingColor ="white";
+var drawingColor = "white";
 var brush = "mediumBrush";
-function changeBrush(element){
+function changeBrush(element) {
     console.log(element);
     brush = element;
-
 }
-
 function changeDrawingColor(eventObject) {
     var element = eventObject;
     document.getElementById('canvas').style.color = element;
@@ -14,16 +12,13 @@ function changeDrawingColor(eventObject) {
     console.log(element);
     drawingColor = element;
 }
-function startBrush(){
-    let arr = ['smallBrush','mediumBrush','giantBrush','miniBrush'];
+function startBrush() {
+    let arr = ['smallBrush', 'mediumBrush', 'giantBrush', 'miniBrush'];
     arr.forEach(element => {
-        document.getElementById(element).addEventListener('click', function(){changeBrush(element)});
+        document.getElementById(element).addEventListener('click', function () { changeBrush(element) });
     });
-
 }
 startBrush();
-
-// init the color
 function startTheColors() {
     let arr = ['red', 'blue', 'green', 'yellow', 'black', 'blueviolet'];
     arr.forEach(element => {
@@ -32,27 +27,46 @@ function startTheColors() {
     });
 }
 startTheColors();
-// draw in the canvas
-
-  canvas.addEventListener('mousedown', makeDot);
-//   canvas.addEventListener('mousedown', setInterval(makeDot),100);
-document.getElementById('canvas').addEventListener('mouseup',clearInterval(makeDot));
-
-function makeDot(e) {
-    
-    var dot = document.createElement("span");
-    dot.className += brush;
-    canvas.appendChild(dot);
-    dot.style.top = e.pageY-e.target.offsetTop +"px";
-    dot.style.left =e.pageX - e.target.offsetLeft +"px";
-    dot.style.backgroundColor = drawingColor;
-    console.log(dot);
-     
-  
+canvas.addEventListener('mousemove', makeDot);
+var mouseDown = 0;
+document.body.onmousedown = function () {
+    ++mouseDown;
+}
+document.body.onmouseup = function () {
+    --mouseDown;
 }
 
-// clear canvas:
- document.getElementById("clear").addEventListener('click', clearCanvas);
- function clearCanvas(){
-     location.reload();
- }
+function makeDot(e) {
+    if (mouseDown) {
+        var dot = document.createElement("span");
+        dot.className += brush;
+        canvas.appendChild(dot);
+        dot.style.top = e.pageY - e.target.offsetTop + "px";
+        dot.style.left = e.pageX - e.target.offsetLeft + "px";
+        dot.style.backgroundColor = drawingColor;
+        console.log(dot);
+    }
+}
+document.getElementById("clear").addEventListener('click', clearCanvas);
+function clearCanvas() {
+    location.reload();
+}
+document.getElementById("erase").addEventListener('click', erase);
+function erase() {
+    
+    canvas.addEventListener('mousemove', eraseDots);
+}
+// function eraseDots(e) {
+//     if (mouseDown) {
+//         canvas.removeEventListener('mousemove', makeDot);
+//         var myDots = document.createElement("span");
+//         myDots.className += 'erase';
+//         canvas.appendChild(myDots);
+//         myDots.style.top = e.pageY - e.target.offsetTop + "px";
+//         myDots.style.left = e.pageX - e.target.offsetLeft + "px";
+//     }
+//     else {
+//         canvas.addEventListener('mousemove', makeDot);
+//         canvas.removeEventListener('mousemove', eraseDots);
+//     }
+// }
