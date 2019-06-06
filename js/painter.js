76680b51b3faf7a6a78c87c349943ff102c37236
Liora <lioralehrer@gmail.com>
2019-06-06 14:20:47 +0300

    making dots by (X,Y)

--- js/painter.js
+++ js/painter.js
@@ -1,20 +1,37 @@
+var canvas = document.getElementById("canvas");
+var drawingColor ="white";
 
-
-//  works like a dream
-function changeDrawingColor(eventObject){
+function changeDrawingColor(eventObject) {
     var element = eventObject;
     document.getElementById('canvas').style.color = element;
     console.log(`i changed the color to ${element} !`);
-    console.log(element); 
+    console.log(element);
+    drawingColor = element;
 }
 
 
-// I love this function .... :)
-function startTheColors(){
-    let arr =['red','blue','green','yellow','black','blueviolet'];
+// init the color
+function startTheColors() {
+    let arr = ['red', 'blue', 'green', 'yellow', 'black', 'blueviolet'];
     arr.forEach(element => {
-        document.getElementById(element).addEventListener('click', function(){changeDrawingColor(element)}  );
+        document.getElementById(element).addEventListener('click', function () { changeDrawingColor(element) });
         console.log(element);
     });
 }
 startTheColors();
+// draw in the canvas
+document.getElementById('canvas').addEventListener('mousedown',makeDot);
+
+function makeDot(e) {
+    
+    var dot = document.createElement("span");
+    dot.className += 'smallRadius';
+    canvas.appendChild(dot);
+    dot.style.top = e.pageY-e.target.offsetTop +"px";
+    dot.style.left =e.pageX - e.target.offsetLeft +"px";
+    dot.style.backgroundColor = drawingColor;
+    console.log(dot);
+
+    
+    
+}
