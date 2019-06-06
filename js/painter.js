7f71e34ab2f0673cd83d7fe5d65ca1c752af686f
Liora <lioralehrer@gmail.com>
2019-06-06 16:28:12 +0300

    size of brushes , clear canvas

--- js/painter.js
+++ js/painter.js
@@ -1,5 +1,11 @@
 var canvas = document.getElementById("canvas");
 var drawingColor ="white";
+var brush = "mediumBrush";
+function changeBrush(element){
+    console.log(element);
+    brush = element;
+
+}
 
 function changeDrawingColor(eventObject) {
     var element = eventObject;
@@ -8,7 +14,14 @@ function changeDrawingColor(eventObject) {
     console.log(element);
     drawingColor = element;
 }
+function startBrush(){
+    let arr = ['smallBrush','mediumBrush','giantBrush','miniBrush'];
+    arr.forEach(element => {
+        document.getElementById(element).addEventListener('click', function(){changeBrush(element)});
+    });
 
+}
+startBrush();
 
 // init the color
 function startTheColors() {
@@ -20,18 +33,26 @@ function startTheColors() {
 }
 startTheColors();
 // draw in the canvas
-document.getElementById('canvas').addEventListener('mousedown',makeDot);
+
+  canvas.addEventListener('mousedown', makeDot);
+//   canvas.addEventListener('mousedown', setInterval(makeDot),100);
+document.getElementById('canvas').addEventListener('mouseup',clearInterval(makeDot));
 
 function makeDot(e) {
     
     var dot = document.createElement("span");
-    dot.className += 'smallRadius';
+    dot.className += brush;
     canvas.appendChild(dot);
     dot.style.top = e.pageY-e.target.offsetTop +"px";
     dot.style.left =e.pageX - e.target.offsetLeft +"px";
     dot.style.backgroundColor = drawingColor;
     console.log(dot);
-
-    
-    
+     
+  
 }
+
+// clear canvas:
+ document.getElementById("clear").addEventListener('click', clearCanvas);
+ function clearCanvas(){
+     location.reload();
+ }
